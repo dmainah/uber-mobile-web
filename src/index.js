2e7ba1214473fab0851c2cd01b93ac37427da423
David Henia <dmainah@gmail.com>
2017-03-01 20:42:09 +0300

    Code reformat

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import icon192 from './images/192.png';
 import favicon from './images/favicon.ico';
 
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { hashHistory, match, Router } from 'react-router';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {hashHistory, match, Router} from 'react-router';
 import routes from './routes';
 
 import './styles/app.scss';
@@ -18,27 +18,27 @@ import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
 let preloadedState = window.__PRELOADED_STATE__;
 if (preloadedState === 'preloadedState') {
-  preloadedState = {};
+    preloadedState = {};
 } else {
-  preloadedState = JSON.parse(preloadedState);
+    preloadedState = JSON.parse(preloadedState);
 }
 
 const store = configureStore(preloadedState);
 
 if ('serviceWorker' in navigator) {
-  const registration = runtime.register();
+    const registration = runtime.register();
 }
 
 
-match({ 
-  'history': hashHistory,
-  routes
+match({
+    'history': hashHistory,
+    routes
 }, (error, redirectLocation, renderProps) => {
 
-  render(
-    <Provider store={store}>
-      <Router {...renderProps} />
-    </Provider>,
-    document.getElementById('app')
-  );
+    render(
+        <Provider store={store}>
+            <Router {...renderProps} />
+        </Provider>,
+        document.getElementById('app')
+    );
 });
